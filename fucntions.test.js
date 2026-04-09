1c6aba26ca2bac0f55df1e891133e0e68cb1c85e
UmaanMehraj <umaanmehraj5@gmail.com>
2026-04-09 16:38:26 +0530

    Add: calculator, strRev

--- fucntions.test.js
+++ fucntions.test.js
@@ -1,5 +1,24 @@
-import { capitalize } from "./capitalize"
+import { capitalize, strRev, Calculator } from './functions'
 
-test('check', ()=>{
-    expect((capitalize('hello'))).toBe('Hello')
+test('capitalize function', () => {
+  expect(capitalize('hello')).toBe('Hello')
+})
+
+test('reverse function', () => {
+  expect(strRev('meh')).toBe('hem')
+})
+
+// Calci object
+const obj = new Calculator()
+
+test('calculator: add', () => {
+  expect(obj.add(1, 2)).toBe(3)
+})
+
+test('calculator: subtraction', () => {
+  expect(obj.subtract(1, 2)).toBe(-1)
+})
+
+test('calculator: multiplication', () => {
+  expect(obj.multiply(3,3)).toBe(9)
 })
--- functions.js
+++ functions.js
@@ -1,3 +1,21 @@
+export class Calculator{
+    add(a, b){
+        return a + b
+    }
+    subtract(a,b){
+        return a - b
+    }
+    divide(a,b){
+        if(b!==0){
+            return a/b
+        }
+    }
+    multiply(a,b){
+        return a * b
+    }
+}
+
+
 export function capitalize(str){
     const char = str.slice(0,1)
     str = char.toUpperCase() + str.slice(1, str.length)
@@ -5,3 +23,7 @@ export function capitalize(str){
     return str
 }
 
+export function strRev(str){
+    return str.split('').reverse().join('')
+
+}
